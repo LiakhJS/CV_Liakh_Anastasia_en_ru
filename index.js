3ede891aeb3311d7817d3a8b6cd536fe2842662b
LiakhJS <anastasialiakh@hotmail.com>
2022-10-06 12:10:52 +0300

    cover

--- index.js
+++ index.js
@@ -212,21 +212,3 @@ resumeItemScaleBlock.addEventListener('click', changeScale);
 resumeItemScaleBlock.addEventListener('touchstart', changeScale);
 
 
-if (mediaQuery640) {
-  window.addEventListener("orientationchange", () => {
-    document.querySelector('#developer').classList.add('display-landscape');
-    document.querySelector('header').classList.add('landscape');
-    document.querySelector('.developer-container').classList.add('landscape');
-    document.querySelector('.mobile-nav-toggle').classList.add('landscape');
-    document.querySelector('#about').classList.add('landscape');
-    document.querySelector('#skills').classList.add('landscape');
-    document.querySelector('#resume').classList.add('landscape');
-    document.querySelector('#projects').classList.add('landscape');
-    document.querySelector('footer').classList.add('landscape');
-  })
-
-} else if (!mediaQuery640) {
-  window.addEventListener("orientationchange", () => {
-    document.querySelector('#developer').classList.remove('display-landscape');
-  })
-}
